d9d705c2e24fb34964f6534c93c4ae392ae096be
Sypth <105406568+Sypth@users.noreply.github.com>
2022-11-11 10:42:24 +0800

    Delete schoolweb directory

--- schoolweb/database/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=spcf', 'Alex', 'Ajcamaddo137');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- schoolweb/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include("functions.php");
-
-  $user_data = check_login($pdo);
-  $id = $_GET['id'];
-  delete($id);
